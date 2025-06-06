a5b7a3881c48da9b76565175d0208c8eebf8bec2
Andrew Bruce <me@andrewbruce.net>
2025-06-06 14:28:37 +0100

    Remove Elixir for now
    Also, watch shell.nix in .envrc.

--- shell.nix
+++ shell.nix
@@ -4,12 +4,7 @@ with pkgs;
 
 mkShell {
   packages = [
-    elixir
-    elixir_ls
+    python3
     nixfmt-rfc-style
   ];
-
-  shellHook = ''
-    export ERL_AFLAGS="-kernel shell_history enabled shell_history_file_bytes 1024000"
-  '';
 }
