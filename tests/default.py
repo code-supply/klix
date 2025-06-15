5be1b2c4a8c05b95fe422e985e4171aad7c5bc95
Andrew Bruce <me@andrewbruce.net>
2025-06-15 17:45:39 +0100

    Ensure Klipper service comes up in test VM

--- tests/default.py
+++ tests/default.py
@@ -2,6 +2,7 @@ moonraker_port = 7125
 nginx_port = 80
 printer.wait_for_open_port(port = moonraker_port, timeout = 10)
 printer.wait_for_open_port(port = nginx_port, timeout = 10)
+printer.wait_for_unit("klipper.service")
 printer.wait_for_unit("cage-tty1.service")
 printer.wait_for_unit("moonraker.service")
 printer.fail("journalctl -u moonraker.service --grep 'not a valid config section'")
