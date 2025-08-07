448d794156cad070f9fe1ae7b751f8004b869f7e
Andrew Bruce <me@andrewbruce.net>
2025-08-07 10:24:34 +0100

    Basic config processing

--- modules/klipper/default.nix
+++ modules/klipper/default.nix
@@ -6,9 +6,10 @@
   ...
 }:
 let
-  cfg = config.services.klipper;
-
-  enabledPlugins = lib.filterAttrs (name: pluginCfg: pluginCfg.enable) cfg.plugins;
+  cfg = config.services.klix;
+  enabledPlugins = lib.filterAttrs (
+    name: pluginCfg: pluginCfg.enable
+  ) config.services.klipper.plugins;
   enabledPluginDefinitions = map (name: plugins.${name}) (builtins.attrNames enabledPlugins);
 
   extraPythonPackages =
@@ -21,6 +22,7 @@ let
 in
 {
   options = {
+    services.klix.configDir = lib.mkOption { example = "./klipper"; };
     services.klipper.plugins = builtins.mapAttrs (name: definition: {
       enable = lib.mkEnableOption definition.description;
     }) plugins;
@@ -39,28 +41,43 @@ in
       groups.klipper = { };
     };
 
-    services.klipper = {
-      enable = true;
+    services.klipper =
+      let
+        userConfigDir = cfg.configDir;
+        configs = pkgs.runCommand "create-printer-config" { } ''
+          mkdir $out
+          # all includes get replaced with the user's config dir as base
+          # except: includes prefixed with extras/ get replaced with the klipper drv's extras dir, where plugins live
+          sed '/\[include extras\//! s#\[include \(.*\)\]#[include ${userConfigDir}/\1]#;s#\[include extras/#[include ${config.services.klipper.package}/lib/klipper/extras/#' \
+          < ${userConfigDir}/printer.cfg \
+          > $out/printer.cfg
+        '';
+      in
 
-      logFile = "/var/lib/klipper/klipper.log";
-      user = "klipper";
-      group = "klipper";
+      {
+        enable = true;
 
-      package =
-        (pkgs.klipper.override {
-          inherit extraPythonPackages;
-        }).overrideAttrs
-          {
-            postUnpack = pkgs.lib.concatMapAttrsStringSep "\n" (
-              name: pluginCfg:
-              let
-                plugin = plugins.${name};
-                pluginSrc = inputs.${name};
-                dest = if plugin.configLink ? to then plugin.configLink.to else plugin.configLink.from;
-              in
-              "ln -sfv ${pluginSrc}/${plugin.configLink.from} source/klippy/extras/${dest}"
-            ) enabledPlugins;
-          };
-    };
+        configFile = if (isNull config.services.klipper.settings) then "${configs}/printer.cfg" else null;
+
+        logFile = "/var/lib/klipper/klipper.log";
+        user = "klipper";
+        group = "klipper";
+
+        package =
+          (pkgs.klipper.override {
+            inherit extraPythonPackages;
+          }).overrideAttrs
+            {
+              postUnpack = pkgs.lib.concatMapAttrsStringSep "\n" (
+                name: pluginCfg:
+                let
+                  plugin = plugins.${name};
+                  pluginSrc = inputs.${name};
+                  dest = if plugin.configLink ? to then plugin.configLink.to else plugin.configLink.from;
+                in
+                "ln -sfv ${pluginSrc}/${plugin.configLink.from} source/klippy/extras/${dest}"
+              ) enabledPlugins;
+            };
+      };
   };
 }
