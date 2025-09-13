3cddaeab41d165d383d830719655c2fc9a22b72b
Andrew Bruce <me@andrewbruce.net>
2025-09-13 10:18:02 +0100

    Compile assets in release

--- web/default.nix
+++ web/default.nix
@@ -1,14 +1,38 @@
 {
   beamPackages,
   callPackages,
+  tailwindcss_4,
+  esbuild,
   version,
 }:
 
+let
+  mixNixDeps = callPackages ./deps.nix { };
+in
 beamPackages.mixRelease {
   pname = "klix";
-  inherit version;
 
-  mixNixDeps = callPackages ./deps.nix { };
+  inherit mixNixDeps version;
 
   src = ./.;
+
+  DATABASE_URL = "";
+  SECRET_KEY_BASE = "";
+
+  postBuild = ''
+    tailwind_path="$(mix do \
+      app.config --no-deps-check --no-compile, \
+      eval 'Tailwind.bin_path() |> IO.puts()')"
+    esbuild_path="$(mix do \
+      app.config --no-deps-check --no-compile, \
+      eval 'Esbuild.bin_path() |> IO.puts()')"
+
+    ln -s ${mixNixDeps.heroicons} /build/web/deps/heroicons
+    ln -s ${tailwindcss_4}/bin/tailwindcss "$tailwind_path"
+    ln -s ${esbuild}/bin/esbuild "$esbuild_path"
+
+    mix do \
+      app.config --no-deps-check --no-compile, \
+      assets.deploy --no-deps-check
+  '';
 }
