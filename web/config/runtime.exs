7c153cdc00e0db937560688f651e715a171611e8
Andrew Bruce <me@andrewbruce.net>
2025-09-13 12:04:14 +0100

    Ensure build dir exists

--- web/config/runtime.exs
+++ web/config/runtime.exs
@@ -53,7 +53,9 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST") || "klix.code.supply"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
-  config :klix, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+  config :klix,
+    dns_cluster_query: System.get_env("DNS_CLUSTER_QUERY"),
+    build_dir: "/tmp/klix-build" |> tap(&File.mkdir_p!/1)
 
   config :klix, KlixWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
