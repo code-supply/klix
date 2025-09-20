2d7312f64486393c9c74ec33335925584447783b
Andrew Bruce <me@andrewbruce.net>
2025-09-20 20:43:29 +0100

    Styled 404 page

--- web/lib/klix_web/controllers/error_html.ex
+++ web/lib/klix_web/controllers/error_html.ex
@@ -13,7 +13,7 @@ defmodule KlixWeb.ErrorHTML do
   #   * lib/klix_web/controllers/error_html/404.html.heex
   #   * lib/klix_web/controllers/error_html/500.html.heex
   #
-  # embed_templates "error_html/*"
+  embed_templates "error_html/*"
 
   # The default is to render a plain text page based on
   # the template name. For example, "404.html" becomes
