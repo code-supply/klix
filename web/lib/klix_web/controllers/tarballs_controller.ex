3c60fc39d2310ad87f0b527a8da0181cf85d100c
Andrew Bruce <me@andrewbruce.net>
2025-10-01 23:42:01 +0100

    Tarball controller obtains image from scope

--- web/lib/klix_web/controllers/tarballs_controller.ex
+++ web/lib/klix_web/controllers/tarballs_controller.ex
@@ -1,9 +1,8 @@
 defmodule KlixWeb.TarballsController do
   use KlixWeb, :controller
 
-  def download(conn, %{"uuid" => uuid}) do
-    image = Klix.Images.find!(uuid)
-    {:ok, _snapshot, tarball} = Klix.Images.snapshot(image)
+  def download(conn, _params) do
+    {:ok, _snapshot, tarball} = Klix.Images.snapshot(conn.assigns.current_scope.image)
     send_resp(conn, :ok, tarball)
   end
 end
