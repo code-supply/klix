a151f642027e2be7251941d057496228e6300f63
Andrew Bruce <me@andrewbruce.net>
2026-01-20 10:22:28 +0000

    Use heroicons instead of DaisyUI copypasta

--- web/lib/klix_web/live/build_new_image_live.ex
+++ web/lib/klix_web/live/build_new_image_live.ex
@@ -36,20 +36,7 @@ defmodule KlixWeb.BuildNewImageLive do
       </.form>
 
       <div :if={!@current_scope} role="alert" class="alert alert-info alert-soft mt-6 w-3/4 m-auto">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="h-6 w-6 shrink-0 stroke-current"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          >
-          </path>
-        </svg>
+        <.icon name="hero-information-circle" class="size-8" />
         <p>
           You are not logged in. You can still create images, download and use them.
           To work with multiple printers, please
--- web/lib/klix_web/live/image_live.ex
+++ web/lib/klix_web/live/image_live.ex
@@ -114,19 +114,7 @@ defmodule KlixWeb.ImageLive do
                   <%= cond do %>
                     <% Images.build_failed?(build) -> %>
                       <div role="alert" class="col-span-3 alert alert-error">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="h-6 w-6 shrink-0 stroke-current"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                          />
-                        </svg>
+                        <.icon name="hero-face-frown" class="size-8" />
                         <div>
                           <h3 class="font-bold">Image build failure</h3>
                           <p>{build.error}</p>
