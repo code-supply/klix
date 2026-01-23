42fc20b279d6abb0a38969cda190c06fdefb4a73
Andrew Bruce <me@andrewbruce.net>
2026-01-23 17:35:54 +0000

    Wait longer for open ports in NixOS tests

--- tests/default.py
+++ tests/default.py
@@ -1,7 +1,7 @@
 moonraker_port = 7125
 nginx_port = 80
-printer.wait_for_open_port(port = moonraker_port, timeout = 20)
-printer.wait_for_open_port(port = nginx_port, timeout = 20)
+printer.wait_for_open_port(port = moonraker_port, timeout = 60)
+printer.wait_for_open_port(port = nginx_port, timeout = 60)
 printer.wait_for_unit("klipper.service")
 printer.wait_for_unit("cage-tty1.service")
 printer.wait_for_unit("moonraker.service")
