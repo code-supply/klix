6c022a8c198ef4e90c45f65e1018005b56dc5f1f
Andrew Bruce <me@andrewbruce.net>
2026-02-23 12:06:31 +0000

    Fix postgres users in CI
    Also add "andrew", for local verification.

--- postgres/start.nix
+++ postgres/start.nix
@@ -4,7 +4,14 @@
 }:
 writeShellScriptBin "postgres-start" ''
   [[ -d "$PGHOST" ]] || \
-    ${postgresql_18}/bin/initdb -D "$PGHOST/db"
+    ${postgresql_18}/bin/initdb \
+    --pgdata "$PGHOST/db" \
+    --username postgres
+
+  > "$PGHOST/db/pg_ident.conf"
+  echo "runner_can_be_postgres runner postgres" >> "$PGHOST/db/pg_ident.conf"
+  echo "andrew_can_be_postgres andrew postgres" >> "$PGHOST/db/pg_ident.conf"
+
   ${postgresql_18}/bin/pg_ctl \
     -D "$PGHOST/db" \
     -l "$PGHOST/log" \
